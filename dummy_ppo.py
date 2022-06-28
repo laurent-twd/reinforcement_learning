eca2f5f67928e655e7323e3cb2220d1e4678dce5
Laurent Thanwerdas <laurent.thanwerdas@gmail.com>
2022-06-28 19:39:36 -0400

    Implemented PPO module.

--- dummy_ppo.py
+++ dummy_ppo.py
@@ -1,3 +1,4 @@
+from sqlite3 import paramstyle
 import numpy as np
 from src.data.trading_dataset import TradingDataset
 from src.models.agent import AgentConfig, TradingAgent, TradingCritic
@@ -5,8 +6,15 @@ from src.env.portfolio import Portfolio
 from src.data.replay_buffer import ReplayBuffer
 import random
 from dataclasses import astuple
-from src.env.transaction_cost import TransactionCostConfig, DynamicTransactionCost
+from src.env.transaction_cost import (
+    TransactionCost,
+    TransactionCostConfig,
+    DynamicTransactionCost,
+)
 import torch
+from copy import deepcopy
+from src.models.ppo import PPOConfig, PPO
+from src.models.networks import RecurrentNetwork
 
 n_assets = 5
 T = 10000
@@ -17,44 +25,20 @@ steps_per_episode = 500
 
 dataset = TradingDataset(X, n_assets, C, 30)
 cost = DynamicTransactionCost(TransactionCostConfig())
-agent = TradingAgent(AgentConfig(n_assets=n_assets))
-critic = TradingCritic(AgentConfig())
 
-env = Portfolio(
-    dataset=dataset,
-    transaction_cost=cost,
-    steps_per_episode=steps_per_episode,
-    name="portfolio",
+agent = TradingAgent(
+    AgentConfig(n_assets=n_assets),
+    network=RecurrentNetwork(n_assets * C, 32, 2 * (n_assets + 1), 2),
 )
-n_trajectories = 16
-n_episodes = 10
-counter = 0
-gamma = 0.99
 
-episode = 0
-print(episode)
-rewards = []
-is_terminal = False
-buffer = ReplayBuffer(steps_per_episode + 10)
-starting_step = torch.randint(0, len(dataset), [n_trajectories])
-env.reset(starting_step)
-values = []
-while not is_terminal:
-    state = env.get_current_state()
-    values.append(critic(state))
-    action = agent(state)
-    reward, next_state, terminal_state = env.step(action)
-    buffer.add_experience(state, action, reward, next_state, terminal_state)
-    is_terminal = terminal_state.bool().all()
-values = torch.cat(values, dim=-1).T
-trajectories = buffer.get_all()
-terminal_states = trajectories.terminal_state
-rewards = trajectories.reward
-powers = torch.arange(0, rewards.shape[0])
-discounted_rewards = rewards * torch.pow(gamma, powers).unsqueeze(-1)
-mask = 1.0 - torch.concat(
-    [torch.zeros(1, terminal_states.shape[-1]), terminal_states[:-1]]
+critic = TradingCritic(
+    AgentConfig(n_assets=n_assets),
+    network=RecurrentNetwork(n_assets * C, 32, 1, 2),
 )
-mean_reward = (rewards.sum(0) / mask.sum(0)).mean(0)
 
-values
+ppo = PPO(config=PPOConfig(lr=1e-5), agent=agent, critic=critic, transaction_cost=cost)
+ppo.fit(dataset, n_episodes=100)
+
+batch_size = 16
+steps_per_episode = 500
+self = ppo
--- src/models/ppo.py
+++ src/models/ppo.py
@@ -1,5 +1,148 @@
+from copy import deepcopy
 import torch
+from src.models.agent import TradingAgent, TradingCritic
+from src.env.portfolio import Portfolio
+from dataclasses import dataclass
+from src.data.trading_dataset import TradingDataset
+from src.data.replay_buffer import ReplayBuffer
+from src.env.transaction_cost import DynamicTransactionCost
+from typing import Union
+from tqdm import tqdm
 
 
-def advantage(rewards: torch.Tensor, values: torch.Tensor, gamma: float = 0.99):
-    pass
+def clipped_loss(rewards, values, ratios, gamma, lambda_, epsilon):
+    delta = rewards[:, :-1] + gamma * values[:, 1:] - values[:, :-1]
+    powers = torch.arange(0, delta.shape[1])
+    advantage = torch.pow((gamma * lambda_), powers).unsqueeze(0) * delta
+    g = torch.where(
+        advantage >= 0, (1.0 + epsilon) * advantage, (1.0 - epsilon) * advantage
+    )
+    loss = torch.minimum(ratios[:, :-1] * advantage, g)
+    return -loss.mean()
+
+
+@dataclass
+class PPOConfig:
+    lr: float = 1e-4
+    gamma: float = 0.99
+    lambda_: float = 0.5
+    epsilon: float = 0.2
+    steps_per_episode: float = 500
+
+
+class PPO:
+    def __init__(
+        self,
+        config: PPOConfig,
+        agent: TradingAgent,
+        critic: TradingCritic,
+        transaction_cost: DynamicTransactionCost,
+    ):
+        self.config = config
+
+        def initialize(model):
+            def init(x):
+                try:
+                    return torch.nn.init.xavier_uniform(x)
+                except:
+                    return torch.nn.init.zeros_(x)
+
+            _ = [init(x) for x in model.network.parameters()]
+            return model
+
+        self.agent = initialize(agent)
+        self.critic = initialize(critic)
+        self.old_agent = initialize(deepcopy(agent))
+        self.old_critic = initialize(deepcopy(critic))
+        self.agent_optimizer = torch.optim.Adam(
+            self.agent.network.parameters(), self.config.lr
+        )
+        self.critic_optimizer = torch.optim.Adam(
+            self.critic.network.parameters(), self.config.lr
+        )
+        self.transaction_cost = transaction_cost
+
+    def run_episode(
+        self, env, starting_step: Union[int, torch.IntTensor], buffer: ReplayBuffer
+    ):
+        env.reset(starting_step)
+        is_terminal = False
+        values = []
+        ratios = []
+
+        while not is_terminal:
+            state = env.get_current_state()
+            inputs = state  # .reshape(state.shape[0], -1)
+            with torch.no_grad():
+                values.append(self.old_critic(inputs))
+                old_parameters = self.old_agent(inputs)
+                old_mean, old_logvar = torch.split(
+                    old_parameters, old_parameters.shape[-1] // 2, dim=-1
+                )
+                old_policy = torch.distributions.Normal(
+                    old_mean, old_logvar.mul(0.5).exp()
+                )
+                action = old_policy.sample().softmax(-1)
+            parameters = self.agent(inputs)
+            mean, logvar = torch.split(parameters, parameters.shape[-1] // 2, dim=-1)
+            policy = torch.distributions.Normal(mean, logvar.mul(0.5).exp())
+            ratio = (
+                (policy.log_prob(action) - old_policy.log_prob(action))
+                .exp()
+                .prod(dim=-1)
+                .unsqueeze(-1)
+            )
+            ratios.append(ratio)
+            reward, next_state, terminal_state = env.step(action)
+            buffer.add_experience(state, action, reward, next_state, terminal_state)
+            is_terminal = terminal_state.bool().all()
+        values = torch.cat(values, dim=-1)
+        ratios = torch.cat(ratios, dim=-1)
+        trajectories = buffer.get_all()
+        rewards = trajectories.reward.T
+
+        return rewards, values, ratios
+
+    def fit(
+        self,
+        dataset: TradingDataset,
+        batch_size: int = 16,
+        steps_per_episode: int = 500,
+        n_episodes: int = 1000,
+    ):
+        env = Portfolio(
+            dataset=dataset,
+            transaction_cost=self.transaction_cost,
+            steps_per_episode=steps_per_episode,
+            name="portfolio",
+        )
+
+        progbar = tqdm(range(n_episodes), desc="Episode ")
+        for _ in progbar:
+            starting_step = torch.randint(
+                0, len(dataset) - steps_per_episode, [batch_size]
+            )
+            buffer = ReplayBuffer(steps_per_episode)
+
+            self.agent_optimizer.zero_grad()
+            self.critic_optimizer.zero_grad()
+
+            rewards, values, ratios = self.run_episode(env, starting_step, buffer)
+            loss = clipped_loss(
+                rewards,
+                values.squeeze(),
+                ratios,
+                self.config.gamma,
+                self.config.lambda_,
+                self.config.epsilon,
+            )
+
+            loss.backward()
+
+            self.old_agent.network.load_state_dict(self.agent.network.state_dict())
+            self.old_critic.network.load_state_dict(self.critic.network.state_dict())
+
+            self.agent_optimizer.step()
+            self.critic_optimizer.step()
+
+            progbar.set_description("Loss: {:e}".format(loss.detach().cpu()))
