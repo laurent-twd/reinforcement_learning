4cb59bada047b843b5968a62105a126b562ec2e6
Laurent Thanwerdas <laurent.thanwerdas@gmail.com>
2022-07-06 00:56:08 -0400

    Updated dummy_ppo.py

--- dummy_ppo.py
+++ dummy_ppo.py
@@ -7,10 +7,11 @@ from src.env.transaction_cost import (
     ConstantTransactionCost,
 )
 from src.models.ppo.ppo import PPOConfig, PPO
-from src.models.networks import RecurrentNetwork
+from src.models.networks import RecurrentNetwork, MLP
 import pandas as pd
 from src.env.portfolio import Portfolio
 import torch
+import numpy as np
 
 M = 365 * 2 * (60 * 24)
 path_to_data = "/Users/laurentthanwerdas/Downloads/CryptoCurrency Historical Prices/BTC Historical Prices/Binance_BTCUSDT_1hour.csv"
@@ -18,84 +19,88 @@ df = pd.read_csv(path_to_data).tail(M)
 columns = ["High", "Low", "Close", "Open"]
 columns = [c.lower() for c in columns]
 df = df.dropna(subset=columns)
-norm = df[columns[-1]].values
-prices = df[columns[:3]].values
+
+
+generated_prices = np.cos(0.1 * np.arange(0, df.shape[0] + 1)) + 10.0
+prices = generated_prices[:-1][:, np.newaxis]
+norm = generated_prices[1:]
+
+
+# norm = df[columns[-1]].values
+# prices = df[columns[:3]].values
 
 n_assets = 1
-n_channels = 3
-window = 48
-steps_per_episode = 500
+n_channels = 1
+window = 30
 
 dataset = TradingDataset(
-    df=prices, n_assets=n_assets, n_channels=n_channels, window=30, norm=norm
+    df=prices, n_assets=n_assets, n_channels=n_channels, window=window, norm=norm
 )
 cost = ConstantTransactionCost(TransactionCostConfig(c_b=0.0, c_s=0.0))
 
 actor = TradingAgent(
     AgentConfig(n_assets=n_assets),
-    network=RecurrentNetwork(n_assets * n_channels, 32, n_assets + 1, 2),
+    network=MLP(n_assets * n_channels * window, 32, n_assets + 1, 3),
 )
 
 critic = TradingCritic(
     AgentConfig(n_assets=n_assets),
-    network=RecurrentNetwork(n_assets * n_channels, 32, 1, 1),
+    network=MLP(n_assets * n_channels * window, 32, 1, 3),
 )
 
-self = PPO(
-    config=PPOConfig(),
+ppo = PPO(
+    config=PPOConfig(
+        lr_actor=1e-4,
+        lr_critic=1e-4,
+        c_1=1e-3,
+        c_2=0.0,
+        steps_per_trajectory=90,
+        n_trajectories=2048,
+        n_epochs=5,
+        batch_size=1024,
+        lambda_=0.0,
+        gamma=0.99,
+        normalize_advantage=False,
+    ),
     actor=actor,
     critic=critic,
     transaction_cost=cost,
 )
 
+ppo.fit(dataset, n_episodes=100)
+
+
+batch_size = 1
+
 env = Portfolio(
     dataset=dataset,
-    transaction_cost=self.transaction_cost,
-    steps_per_episode=self.config.steps_per_trajectory,
+    transaction_cost=ppo.transaction_cost,
+    steps_per_episode=ppo.config.steps_per_trajectory,
     name="portfolio",
 )
 
+starting_step = torch.randint(0, len(dataset) - ppo.config.steps_per_trajectory, [1])
 
-starting_step = torch.randint(
-    0,
-    len(dataset) - self.config.steps_per_trajectory,
-    [self.config.n_trajectories],
-)
+starting_step = starting_step + torch.zeros(512).long()
+
+env.reset(starting_step)
+ppo.buffer.reset()
+log_probs, values = ppo.run_episode(env)
+trajectories = ppo.buffer.collate([ppo.buffer[i] for i in range(len(ppo.buffer))])
+rewards = trajectories[1].reward
+actions = trajectories[1].action
+portfolio_value = rewards.exp().prod(-1)
+_, l0, l1 = dataset[starting_step[0] + torch.arange(0, rewards.shape[0])]
+call = (l1 / l0)[:, 0].prod(-1)
+(portfolio_value > call).float().mean()
 
 
-# batch_size = 100
-# from src.env.portfolio import Portfolio
-# from src.data.replay_buffer import ReplayBuffer
-# import torch
+import matplotlib.pyplot as plt
 
-# env = Portfolio(
-#     dataset=dataset,
-#     transaction_cost=ppo.transaction_cost,
-#     steps_per_episode=steps_per_episode,
-#     name="portfolio",
-# )
+y1 = prices[starting_step[0] + np.arange(0, ppo.config.steps_per_trajectory), 0]
+y2 = actions.mean(0)[:, 1]
 
-# starting_step = torch.randint(0, len(dataset) - steps_per_episode, [batch_size])
-# env.reset(starting_step)
-# buffer = ReplayBuffer(steps_per_episode)
-# rewards, values, ratios = ppo.run_episode(env, starting_step, buffer)
-# self = ppo
-# gamma = ppo.config.gamma
-# lambda_ = ppo.config.lambda_
-# epsilon = ppo.config.epsilon
-# c_1 = ppo.config.c_1
-# values = values.squeeze()
-
-# rewards.exp().prod(-1).mean()
-
-# portfolio_value = rewards.exp().prod(-1)
-# _, l0, l1 = dataset[starting_step + torch.arange(0, rewards.shape[0])]
-# call = (l1 / l0).prod(-1)
-# (portfolio_value > call).float().mean()
-
-# # weights = buffer.get_all().action
-# # action = weights.diff(dim=0)[:, -1] > 0
-# # labels = (l1 - l0 > 0)[1:, 0]
-# # assert labels.shape == action.shape
-
-# # print(f"Edge: {(action == labels).float().mean()}")
+plt.plot(((y1 - 10) + 1.0) / 2)
+plt.plot(y2)
+plt.show()
+plt.savefig("test.png")
