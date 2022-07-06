d6e7e1d43988565e9edf2b50f5cc22b0e95c82bc
Laurent Thanwerdas <laurent.thanwerdas@gmail.com>
2022-07-06 00:54:53 -0400

    Added MLP to networks.

--- src/models/networks.py
+++ src/models/networks.py
@@ -1,6 +1,37 @@
 import torch
 
 
+class MLP(torch.nn.Module):
+    def __init__(
+        self, input_size: int, hidden_size: int, output_size: int, n_layers: int
+    ):
+        super().__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.n_layers = n_layers
+        self.layers = torch.nn.ModuleList()
+        for i in range(n_layers):
+            if i == 0:
+                self.layers.append(torch.nn.Linear(input_size, hidden_size))
+            else:
+                self.layers.append(torch.nn.Linear(hidden_size, hidden_size))
+            self.layers.append(torch.nn.ReLU())
+        self.output_layer = torch.nn.Linear(
+            in_features=hidden_size, out_features=output_size
+        )
+
+    def __call__(self, x):
+        y = torch.nn.Flatten(start_dim=1, end_dim=-1)(x)
+        for i, layer in enumerate(self.layers):
+            y = layer(y)
+        output = self.output_layer(y)
+        if self.output_size == 1:
+            return output
+        else:
+            return output.squeeze()
+
+
 class RecurrentNetwork(torch.nn.Module):
     def __init__(
         self, input_size: int, hidden_size: int, output_size: int, n_layers: int
